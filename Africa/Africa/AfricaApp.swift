178ca0de10af5e68254e66fa0f3b997147df1369
sepideh asgharpoor hooshiar <sepideh.a.hooshiar@gmail.com>
2026-01-02 08:54:14 +0330

    - Africa project is created.

---
+++ Africa/Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by sepideh aph on 2026-01-02.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
